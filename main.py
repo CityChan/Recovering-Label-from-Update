92b33756a0008183dab6377b58e88ffc0fe2fda4
CityChan <huanchengch@gmail. om>
2024-12-20 10:20:53 -0600

    update

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ from option import args_parser
 from utils import average_weights,global_acc,AverageMeter
 from llg import get_label_stats,get_emb,post_process_emb,get_irlg_res
 args = args_parser()
+print(args)
