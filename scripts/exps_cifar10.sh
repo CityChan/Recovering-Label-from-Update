90e2928f7d112bc6a55582cf83d6efb10bcba94a
CityChan <huanchengch@gmail. om>
2024-12-20 23:42:05 -0600

    update

--- scripts/exps_cifar10.sh
+++ scripts/exps_cifar10.sh
@@ -23,3 +23,16 @@ python main.py --scheme LLGp --local_epoch 10 --dataset CIFAR10 --momentum 0.0 -
 
 # ZLGp
 python main.py --scheme ZLGp --local_epoch 10 --dataset CIFAR10 --momentum 0.0 --alpha 0.5 --batch_size 64 --model vgg16 --hidden 512
+
+# 2. multiple local epochs with activation function relu with momentum
+# iRLG
+python main.py --scheme iRLG --local_epoch 10 --dataset CIFAR10 --momentum 0.9 --alpha 0.5 --batch_size 64 --model vgg16 --hidden 512 --lr 0.05
+
+# RLU
+python main.py --scheme RLU --local_epoch 10 --dataset CIFAR10 --momentum 0.9 --alpha 0.5 --batch_size 64 --model vgg16 --hidden 512 --lr 0.05
+
+# LLGp
+python main.py --scheme LLGp --local_epoch 10 --dataset CIFAR10 --momentum 0.9 --alpha 0.5 --batch_size 64 --model vgg16 --hidden 512 --lr 0.05
+
+# ZLGp
+python main.py --scheme ZLGp --local_epoch 10 --dataset CIFAR10 --momentum 0.9 --alpha 0.5 --batch_size 64 --model vgg16 --hidden 512 --lr 0.05
